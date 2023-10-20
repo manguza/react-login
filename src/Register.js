ff0a68c1ce090f2f827d51a498bb3350556b1ebf
Pruk Homnan <55727979+manguza@users.noreply.github.com>
2023-10-20 21:30:01 +0700

    20/10/2023-2

--- src/Register.js
+++ src/Register.js
@@ -17,7 +17,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://www.facebook.com/pruk.mtc/">
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
